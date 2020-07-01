71b3c0d574114ebd1954c4af653ce6ec7691367f
jc-ha <jchcodes@gmail.com>
2020-07-01 05:59:17 +0000

    edits

--- exercises101/easy1-7.js
+++ exercises101/easy1-7.js
@@ -3,7 +3,7 @@
 function shortLongShort(str1, str2) {
   if (str1.length > str2.length) {
     return str2 + str1 + str2;
-  } else if (str1.length < str2.length) {
+  } else {
     return str1 + str2 + str1;
   }
 }
