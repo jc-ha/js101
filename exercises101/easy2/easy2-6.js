77759c8c5af022015ed6939f0d27e87a77fa2f1e
jc-ha <jchcodes@gmail.com>
2020-07-04 03:45:02 +0000

    easy2-6

---
+++ exercises101/easy2/easy2-6.js
@@ -0,0 +1,22 @@
+// easy2-6.js
+
+function penultimate(string) {
+  let arrWords = string.split(' ');
+  return arrWords[arrWords.length - 2];
+}
+
+console.log(penultimate("last word") === "last"); // logs true
+console.log(penultimate("Launch School is great!") === "is"); // logs true
+console.log(penultimate('Let us go to the market tomorrow night.'));
+
+function middleWord(string) {
+  let arrWords = string.split(' ');
+
+  if (arrWords.length < 2) {
+    return arrWords[0];
+  } else {
+    return arrWords[Math.floor(arrWords.length / 2)];
+  }
+}
+
+console.log(middleWord('Let us go to the market tomorrow night k.'));
