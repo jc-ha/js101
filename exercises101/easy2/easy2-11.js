8611c62e24de154be909b3dfe118d6df07b4a655
jc-ha <jchcodes@gmail.com>
2020-07-05 03:10:41 +0000

    easy2-11

--- exercises101/easy2/easy2-11.js
+++ exercises101/easy2/easy2-11.js
@@ -1,30 +1,37 @@
 // easy2-11.js
 
-/*function integerToString(integer) {
-  return `${integer}`;
-}*/
-
 const DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 function integerToString(integer) {
-  let digits = [];
-  
-  let number = integer;
-  while (number > 0) {
-    digits.push(number % 10);
-    number -= (number % 10);
-  }
-  
-  console.log(digits);
+  let stringDigits = [];
+
+  do {
+    let digits = integer % 10;
+    stringDigits.push(DIGITS[digits]);
+    integer = (integer - digits) / 10;
+  } while (integer > 0);
+
+  return stringDigits.reverse().join('');
 }
 
-integerToString(4321);
+function integerToString1(integer) {
+  let result = '';
 
-/*
-let number = 4936;
-let ones = number % 10;
-number = (number - ones) / 10;
-let tens = number % 10;
-number = (number - tens) / 10;
-let hundreds = number % 10;
-let thousands = (number - hundreds) / 10; */
+  do {
+    let digits = integer % 10;
+    integer = Math.floor(integer / 10);
+
+    result = DIGITS[digits] + result;
+  } while (integer > 0);
+
+  return result;
+}
+
+console.log(integerToString(4321));      // "4321"
+console.log(integerToString(0));         // "0"
+console.log(integerToString(5000));      // "5000"
+console.log(integerToString(1234567890));      // "1234567890"
+console.log(integerToString1(4321));      // "4321"
+console.log(integerToString1(0));         // "0"
+console.log(integerToString1(5000));      // "5000"
+console.log(integerToString1(1234567890));      // "1234567890"
