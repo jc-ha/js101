280b19b9f117aa5fc5834464f121750e017d3d09
jc-ha <jchcodes@gmail.com>
2020-07-05 06:43:14 +0000

    notes

--- exercises101/easy3/easy3-1.js
+++ exercises101/easy3/easy3-1.js
@@ -1,2 +1,19 @@
 // easy3-1.js
 
+function crunch(string) {
+  let condensedString = '';
+
+  for (let index = 0; index < string.length; index += 1) {
+    if (string[index] !== string[index - 1]) {
+      condensedString += string[index];
+    }
+  }
+
+  return condensedString;
+}
+
+console.log(crunch('ddaaiillyy ddoouubbllee'));    // "daily double"
+console.log(crunch('4444abcabccba'));              // "4abcabcba"
+console.log(crunch('ggggggggggggggg'));            // "g"
+console.log(crunch('a'));                          // "a"
+console.log(crunch(''));                           // ""
