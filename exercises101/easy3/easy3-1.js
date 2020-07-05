c910bcc8b23243f6657427c5e15a11cc803964e1
jc-ha <jchcodes@gmail.com>
2020-07-05 06:57:17 +0000

    easy3-1

--- exercises101/easy3/easy3-1.js
+++ exercises101/easy3/easy3-1.js
@@ -1,6 +1,6 @@
 // easy3-1.js
 
-function crunch(string) {
+/*function crunch(string) {
   let condensedString = '';
 
   for (let index = 0; index < string.length; index += 1) {
@@ -10,6 +10,21 @@ function crunch(string) {
   }
 
   return condensedString;
+}*/
+
+function crunch(text) {
+  let index = 0;
+  let crunchText = '';
+
+  while (index <= text.length - 1) {
+    if (text[index] !== text[index + 1]) {
+      crunchText += text[index];
+    }
+
+    index += 1;
+  }
+
+  return crunchText;
 }
 
 console.log(crunch('ddaaiillyy ddoouubbllee'));    // "daily double"
@@ -17,3 +32,12 @@ console.log(crunch('4444abcabccba'));              // "4abcabcba"
 console.log(crunch('ggggggggggggggg'));            // "g"
 console.log(crunch('a'));                          // "a"
 console.log(crunch(''));                           // ""
+
+/** The last index character needs to be iterated over because the
+ * previous character may have been passed over if the last two
+ * characters matched. For example, with ddoouubbllee, the first e
+ * was ignored because there was a second e, but if the loop stopped
+ * when it got to index === text.length - 1, then it wouldn't iterate
+ * over the last character and that e character would not be included.
+ * Since text[index + 1] is undefined and e !== undefined, the last e
+ * will be added to crunchText and you get 'daily double'. */
