e75965982ea9b62fdc5d2e85c18f957bd1a641b0
jc-ha <jchcodes@gmail.com>
2020-07-18 09:38:38 +0000

    leasy

---
+++ lesson_3/leasy2/leasy2-8.js
@@ -0,0 +1,7 @@
+// leasy2-8.js
+
+let numbers = [1, 2, 3, 4]; // true
+let table = { field1: 1, field2: 2, field3: 3, field4: 4 }; // false
+
+console.log(Array.isArray(numbers));
+console.log(Array.isArray(table));
