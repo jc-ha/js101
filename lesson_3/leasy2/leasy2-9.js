818c77cb009708103a413c87d8771286154e2712
jc-ha <jchcodes@gmail.com>
2020-07-18 21:25:59 +0000

    leasy2-9

--- lesson_3/leasy2/leasy2-9.js
+++ lesson_3/leasy2/leasy2-9.js
@@ -1,2 +1,10 @@
 // leasy2-9.js
 
+let title = "Flintstone Family Members";
+
+let padding = Math.floor((40 - title.length) / 2);
+
+let title1 =
+  title.padStart(title.length + padding, ' ');
+console.log(title1);
+console.log(title1.length);
