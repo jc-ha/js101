5b0a5bc66268b4391e16d9bf761c17fa26bff191
jc-ha <jchcodes@gmail.com>
2020-07-23 18:46:20 +0000

    leasy3-2

---
+++ lesson_3/leasy3/leasy3-2.js
@@ -0,0 +1,7 @@
+// leasy3-2.js
+
+console.log([1, 2, 3] + [4, 5]);
+
+/* + operator converts arrays into strings, concatenates them - when 
+one operand is an object, both operands are converted to strings and 
+concatenated */
