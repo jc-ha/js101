9ad7767be84618cfe2ac1db20923b11fcb02c644
jc-ha <jchcodes@gmail.com>
2020-07-25 21:19:14 +0000

    leasy3-5

---
+++ lesson_3/leasy3/leasy3-5.js
@@ -0,0 +1,17 @@
+// leasy3-5.js
+
+function isColorValid(color) {
+  if (color === "blue" || color === "green") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isColorValid1(color) {
+  return (color === 'blue' || color === 'green');
+}
+
+let isColorValid2 = color => color === 'blue' || color === 'green';
+
+let isColorValid3 = color => ['blue', 'green'].includes(color);
