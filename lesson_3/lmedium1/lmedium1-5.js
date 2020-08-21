4f858117dc2edab37fadb6a214cb8e378d52000b
jc-ha <jchcodes@gmail.com>
2020-08-21 18:22:44 +0000

    lmedium1-5

---
+++ lesson_3/lmedium1/lmedium1-5.js
@@ -0,0 +1,8 @@
+// lmedium1-5.js
+
+console.log(0.3 + 0.6); // 0.8999999999999999
+console.log(0.3 + 0.6 === 0.9); // false
+
+/* JS uses floating point numbers for all numeric operations
+and most floating point representations used on computers lack
+precision, yielding unexpected results like this */
